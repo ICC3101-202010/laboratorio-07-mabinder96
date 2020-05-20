9cc2c0d7f4ae6297d1e080f09de05921cdf19bed
Maria Francisca Binder Correa <mbinder@miuandes.cl>
2020-05-20 17:49:32 -0400

    finalsinbonus

---
+++ Laboratorio 7/Laboratorio 7/Calculator.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace Laboratorio_7
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            this.N7 = new System.Windows.Forms.Button();
+            this.N3 = new System.Windows.Forms.Button();
+            this.N8 = new System.Windows.Forms.Button();
+            this.N2 = new System.Windows.Forms.Button();
+            this.N1 = new System.Windows.Forms.Button();
+            this.N6 = new System.Windows.Forms.Button();
+            this.N5 = new System.Windows.Forms.Button();
+            this.N4 = new System.Windows.Forms.Button();
+            this.N9 = new System.Windows.Forms.Button();
+            this.N0 = new System.Windows.Forms.Button();
+            this.Decimal = new System.Windows.Forms.Button();
+            this.igual = new System.Windows.Forms.Button();
+            this.suma = new System.Windows.Forms.Button();
+            this.resta = new System.Windows.Forms.Button();
+            this.multiplicar = new System.Windows.Forms.Button();
+            this.dividir = new System.Windows.Forms.Button();
+            this.history = new System.Windows.Forms.Button();
+            this.AC = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.dell = new System.Windows.Forms.Button();
+            this.ans = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.operationlebel = new System.Windows.Forms.Label();
+            this.historialpanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // N7
+            // 
+            this.N7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N7.Location = new System.Drawing.Point(22, 173);
+            this.N7.Name = "N7";
+            this.N7.Size = new System.Drawing.Size(75, 30);
+            this.N7.TabIndex = 0;
+            this.N7.Text = "7";
+            this.N7.UseVisualStyleBackColor = true;
+            this.N7.Click += new System.EventHandler(this.N_Click);
+            // 
+            // N3
+            // 
+            this.N3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N3.Location = new System.Drawing.Point(250, 284);
+            this.N3.Name = "N3";
+            this.N3.Size = new System.Drawing.Size(75, 30);
+            this.N3.TabIndex = 1;
+            this.N3.Text = "3";
+            this.N3.UseVisualStyleBackColor = true;
+            this.N3.Click += new System.EventHandler(this.N_Click);
+            // 
+            // N8
+            // 
+            this.N8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N8.Location = new System.Drawing.Point(135, 173);
+            this.N8.Name = "N8";
+            this.N8.Size = new System.Drawing.Size(75, 30);
+            this.N8.TabIndex = 2;
+            this.N8.Text = "8";
+            this.N8.UseVisualStyleBackColor = true;
+            this.N8.Click += new System.EventHandler(this.N_Click);
+            // 
+            // N2
+            // 
+            this.N2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N2.Location = new System.Drawing.Point(135, 284);
+            this.N2.Name = "N2";
+            this.N2.Size = new System.Drawing.Size(75, 30);
+            this.N2.TabIndex = 3;
+            this.N2.Text = "2";
+            this.N2.UseVisualStyleBackColor = true;
+            this.N2.Click += new System.EventHandler(this.N_Click);
+            // 
+            // N1
+            // 
+            this.N1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N1.Location = new System.Drawing.Point(22, 284);
+            this.N1.Name = "N1";
+            this.N1.Size = new System.Drawing.Size(75, 30);
+            this.N1.TabIndex = 4;
+            this.N1.Text = "1";
+            this.N1.UseVisualStyleBackColor = true;
+            this.N1.Click += new System.EventHandler(this.N_Click);
+            // 
+            // N6
+            // 
+            this.N6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N6.Location = new System.Drawing.Point(250, 230);
+            this.N6.Name = "N6";
+            this.N6.Size = new System.Drawing.Size(75, 30);
+            this.N6.TabIndex = 5;
+            this.N6.Text = "6";
+            this.N6.UseVisualStyleBackColor = true;
+            this.N6.Click += new System.EventHandler(this.N_Click);
+            // 
+            // N5
+            // 
+            this.N5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N5.Location = new System.Drawing.Point(135, 230);
+            this.N5.Name = "N5";
+            this.N5.Size = new System.Drawing.Size(75, 30);
+            this.N5.TabIndex = 6;
+            this.N5.Text = "5";
+            this.N5.UseVisualStyleBackColor = true;
+            this.N5.Click += new System.EventHandler(this.N_Click);
+            // 
+            // N4
+            // 
+            this.N4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N4.Location = new System.Drawing.Point(22, 230);
+            this.N4.Name = "N4";
+            this.N4.Size = new System.Drawing.Size(75, 30);
+            this.N4.TabIndex = 7;
+            this.N4.Text = "4";
+            this.N4.UseVisualStyleBackColor = true;
+            this.N4.Click += new System.EventHandler(this.N_Click);
+            // 
+            // N9
+            // 
+            this.N9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N9.Location = new System.Drawing.Point(250, 173);
+            this.N9.Name = "N9";
+            this.N9.Size = new System.Drawing.Size(75, 30);
+            this.N9.TabIndex = 8;
+            this.N9.Text = "9";
+            this.N9.UseVisualStyleBackColor = true;
+            this.N9.Click += new System.EventHandler(this.N_Click);
+            // 
+            // N0
+            // 
+            this.N0.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N0.Location = new System.Drawing.Point(135, 342);
+            this.N0.Name = "N0";
+            this.N0.Size = new System.Drawing.Size(75, 30);
+            this.N0.TabIndex = 9;
+            this.N0.Text = "0";
+            this.N0.UseVisualStyleBackColor = true;
+            this.N0.Click += new System.EventHandler(this.N_Click);
+            // 
+            // Decimal
+            // 
+            this.Decimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Decimal.Location = new System.Drawing.Point(354, 394);
+            this.Decimal.Name = "Decimal";
+            this.Decimal.Size = new System.Drawing.Size(75, 38);
+            this.Decimal.TabIndex = 10;
+            this.Decimal.Text = ",";
+            this.Decimal.UseVisualStyleBackColor = true;
+            this.Decimal.Click += new System.EventHandler(this.N_Click);
+            // 
+            // igual
+            // 
+            this.igual.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.igual.Location = new System.Drawing.Point(250, 342);
+            this.igual.Name = "igual";
+            this.igual.Size = new System.Drawing.Size(75, 90);
+            this.igual.TabIndex = 11;
+            this.igual.Text = "=";
+            this.igual.UseVisualStyleBackColor = true;
+            this.igual.Click += new System.EventHandler(this.igual_Click);
+            // 
+            // suma
+            // 
+            this.suma.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.suma.Location = new System.Drawing.Point(354, 284);
+            this.suma.Name = "suma";
+            this.suma.Size = new System.Drawing.Size(75, 30);
+            this.suma.TabIndex = 12;
+            this.suma.Text = "+";
+            this.suma.UseVisualStyleBackColor = true;
+            this.suma.Click += new System.EventHandler(this.Operation);
+            // 
+            // resta
+            // 
+            this.resta.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resta.Location = new System.Drawing.Point(354, 230);
+            this.resta.Name = "resta";
+            this.resta.Size = new System.Drawing.Size(75, 30);
+            this.resta.TabIndex = 13;
+            this.resta.Text = "-";
+            this.resta.UseVisualStyleBackColor = true;
+            this.resta.Click += new System.EventHandler(this.Operation);
+            // 
+            // multiplicar
+            // 
+            this.multiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiplicar.Location = new System.Drawing.Point(354, 173);
+            this.multiplicar.Name = "multiplicar";
+            this.multiplicar.Size = new System.Drawing.Size(75, 30);
+            this.multiplicar.TabIndex = 14;
+            this.multiplicar.Text = "x";
+            this.multiplicar.UseVisualStyleBackColor = true;
+            this.multiplicar.Click += new System.EventHandler(this.Operation);
+            // 
+            // dividir
+            // 
+            this.dividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dividir.Location = new System.Drawing.Point(354, 342);
+            this.dividir.Name = "dividir";
+            this.dividir.Size = new System.Drawing.Size(75, 30);
+            this.dividir.TabIndex = 15;
+            this.dividir.Text = "÷";
+            this.dividir.UseVisualStyleBackColor = true;
+            this.dividir.Click += new System.EventHandler(this.Operation);
+            // 
+            // history
+            // 
+            this.history.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.history.Location = new System.Drawing.Point(22, 394);
+            this.history.Name = "history";
+            this.history.Size = new System.Drawing.Size(190, 38);
+            this.history.TabIndex = 16;
+            this.history.Text = "HISTORIAL";
+            this.history.UseVisualStyleBackColor = true;
+            this.history.Click += new System.EventHandler(this.history_Click);
+            // 
+            // AC
+            // 
+            this.AC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AC.Location = new System.Drawing.Point(22, 127);
+            this.AC.Name = "AC";
+            this.AC.Size = new System.Drawing.Size(75, 30);
+            this.AC.TabIndex = 17;
+            this.AC.Text = "AC";
+            this.AC.UseVisualStyleBackColor = true;
+            this.AC.Click += new System.EventHandler(this.AC_Click);
+            // 
+            // C
+            // 
+            this.C.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C.Location = new System.Drawing.Point(135, 127);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(75, 30);
+            this.C.TabIndex = 18;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // dell
+            // 
+            this.dell.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dell.Location = new System.Drawing.Point(250, 127);
+            this.dell.Name = "dell";
+            this.dell.Size = new System.Drawing.Size(179, 30);
+            this.dell.TabIndex = 19;
+            this.dell.Text = "Dell";
+            this.dell.UseVisualStyleBackColor = true;
+            this.dell.Click += new System.EventHandler(this.dell_Click);
+            // 
+            // ans
+            // 
+            this.ans.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ans.Location = new System.Drawing.Point(22, 342);
+            this.ans.Name = "ans";
+            this.ans.Size = new System.Drawing.Size(75, 30);
+            this.ans.TabIndex = 20;
+            this.ans.Text = "Ans";
+            this.ans.UseVisualStyleBackColor = true;
+            this.ans.Click += new System.EventHandler(this.ans_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 26);
+            this.textBox1.TabIndex = 21;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // operationlebel
+            // 
+            this.operationlebel.AutoSize = true;
+            this.operationlebel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.operationlebel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operationlebel.Location = new System.Drawing.Point(18, 58);
+            this.operationlebel.Name = "operationlebel";
+            this.operationlebel.Size = new System.Drawing.Size(0, 20);
+            this.operationlebel.TabIndex = 22;
+            // 
+            // historialpanel
+            // 
+            this.historialpanel.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.historialpanel.Location = new System.Drawing.Point(22, 12);
+            this.historialpanel.Name = "historialpanel";
+            this.historialpanel.Size = new System.Drawing.Size(412, 435);
+            this.historialpanel.TabIndex = 23;
+            this.historialpanel.Visible = false;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 459);
+            this.Controls.Add(this.historialpanel);
+            this.Controls.Add(this.operationlebel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ans);
+            this.Controls.Add(this.dell);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.AC);
+            this.Controls.Add(this.history);
+            this.Controls.Add(this.dividir);
+            this.Controls.Add(this.multiplicar);
+            this.Controls.Add(this.resta);
+            this.Controls.Add(this.suma);
+            this.Controls.Add(this.igual);
+            this.Controls.Add(this.Decimal);
+            this.Controls.Add(this.N0);
+            this.Controls.Add(this.N9);
+            this.Controls.Add(this.N4);
+            this.Controls.Add(this.N5);
+            this.Controls.Add(this.N6);
+            this.Controls.Add(this.N1);
+            this.Controls.Add(this.N2);
+            this.Controls.Add(this.N8);
+            this.Controls.Add(this.N3);
+            this.Controls.Add(this.N7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Calculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button N7;
+        private System.Windows.Forms.Button N3;
+        private System.Windows.Forms.Button N8;
+        private System.Windows.Forms.Button N2;
+        private System.Windows.Forms.Button N1;
+        private System.Windows.Forms.Button N6;
+        private System.Windows.Forms.Button N5;
+        private System.Windows.Forms.Button N4;
+        private System.Windows.Forms.Button N9;
+        private System.Windows.Forms.Button N0;
+        private System.Windows.Forms.Button Decimal;
+        private System.Windows.Forms.Button igual;
+        private System.Windows.Forms.Button suma;
+        private System.Windows.Forms.Button resta;
+        private System.Windows.Forms.Button multiplicar;
+        private System.Windows.Forms.Button dividir;
+        private System.Windows.Forms.Button history;
+        private System.Windows.Forms.Button AC;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button dell;
+        private System.Windows.Forms.Button ans;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label operationlebel;
+        private System.Windows.Forms.Panel historialpanel;
+    }
+}
---
+++ Laboratorio 7/Laboratorio 7/Calculator.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio_7
+{
+    public partial class Calculator : Form
+    {
+        decimal result = 0;
+        string br="0";
+        string operation="";
+        bool operperf = false;
+        
+        
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void N_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || operperf==true)
+            {
+                textBox1.Clear();
+
+            }
+            operperf = false;
+            Button button = (Button)sender;
+            textBox1.Text += button.Text;
+        }
+
+        private void dell_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 1)
+            {
+                textBox1.Text =textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            }
+            else
+            {
+                textBox1.Text = "0";
+            }
+        }
+
+        private void Operation(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            
+            operation = button.Text;
+            try
+            {
+                result = decimal.Parse(textBox1.Text);
+                operationlebel.Text = result + " " + operation + " ";
+                operperf = true;
+            }
+            catch (SystemException)
+            {
+                textBox1.Text = "SyntaxERROR";
+            }
+            
+            
+            
+        }
+
+        private void AC_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            br = "0";
+            operationlebel.Text = "";
+            result = 0;
+        }
+
+        private void igual_Click(object sender, EventArgs e)
+        {
+            operationlebel.Text += textBox1.Text;
+            switch (operation)
+            {
+                case "+":
+                    try
+                    {
+                        textBox1.Text = (result + decimal.Parse(textBox1.Text)).ToString();
+                        br = textBox1.Text;
+                        operperf = true;
+
+
+
+                    }
+                    catch (SystemException)
+                    {
+                        textBox1.Text = "SyntaxERROR";
+                        operperf = true;
+                    }
+
+                    break;
+                case "-":
+                    try
+                    {
+                        textBox1.Text = (result - decimal.Parse(textBox1.Text)).ToString();
+                        br = textBox1.Text;
+                        operperf = true;
+
+                    }
+                    catch (SystemException)
+                    {
+                        textBox1.Text = "SyntaxERROR";
+                        operperf = true;
+                    }
+                    break;
+                case "÷":
+                    try
+                    {
+                        textBox1.Text = (result / decimal.Parse(textBox1.Text)).ToString();
+                        br = textBox1.Text;
+                        operperf = true;
+
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        textBox1.Text = "MathERROR";
+                        operperf = true;
+                    }
+                    catch (SystemException)
+                    {
+                        textBox1.Text = "SyntaxERROR";
+                        operperf = true;
+                    }
+
+                    break;
+                case "x":
+                    try
+                    {
+                        textBox1.Text = (result * decimal.Parse(textBox1.Text)).ToString();
+                        br = textBox1.Text;
+                        operperf = true;
+
+
+                    }
+                    catch (SystemException)
+                    {
+                        textBox1.Text = "SyntaxERROR";
+                        operperf = true;
+
+                    }
+                    break;
+                default:
+                    try
+                    {
+                        textBox1.Text = (decimal.Parse(textBox1.Text)).ToString();
+                        br = textBox1.Text;
+                        operperf = true;
+
+                    }
+                    catch (SystemException)
+                    {
+                        textBox1.Text = "SyntaxERROR";
+                        operperf = true;
+                    }
+
+                    break;
+            }
+        }
+        
+
+        private void ans_Click(object sender, EventArgs e)
+        {
+            textBox1.Text =br;
+        }
+
+        private void history_Click(object sender, EventArgs e)
+        {
+            historialpanel.Visible = true;
+        }
+    }
+}
